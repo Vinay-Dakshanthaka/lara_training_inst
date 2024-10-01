5c2c1eae49fb53773ccbb86998c69e8e6137d06c
Vinay <vinayd098@gmail.com>
2024-10-01 10:09:47 +0530

    config

--- lara-frontend/src/components/config.js
+++ lara-frontend/src/components/config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Define baseURL variable
 // export const baseURL = "http://localhost:8080";
 export const baseURL = "https://api.laragrooming.com";
-
+ 
 // Create axios instance with baseURL
 const axiosInstance = axios.create({
   baseURL: baseURL,
